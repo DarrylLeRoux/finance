821adafdbc7190444afa2cf414e226dc37e07186
Darryl Le Roux <devdarryl@outlook.com>
2023-11-02 22:45:07 +0200

    first load

---
+++ index.js
@@ -0,0 +1,22 @@
+const close = document.getElementById('close');
+const modal_container = document.getElementById('modal_container');
+const main = document.querySelector('.main');
+const choice = document.querySelector('.choice');
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+  main.style.display = 'block';
+  choice.style.display = 'flex';
+});
+
+function playSound() {
+  let sound = document.getElementById('audio');
+  sound.play();
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  modal_container.classList.add('show');
+  main.style.display = 'none';
+  choice.style.display = 'none';
+  console.log('DOM fully loaded and parsed');
+});
